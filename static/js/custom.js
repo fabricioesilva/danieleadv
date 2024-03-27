dd6e639c4d5fc5af1570bc6f6f25d08801396e69
root <root@esilva.com.br>
2024-03-26 22:21:36 -0300

    Avancos 260324 2221

---
+++ static/js/custom.js
@@ -0,0 +1,10 @@
+let storeScroll = window.scrollY;
+let htmlTag = document.getElementsByTagName('html');
+document.addEventListener('scroll', () => {
+  storeScroll = window.scrollY;
+  if(storeScroll > 20) {
+    htmlTag[0].setAttribute('data-scroll', 1);
+  } else {
+    htmlTag[0].setAttribute('data-scroll', 0);
+  }
+});
