f81f3dd044bbf2a6044727075df411c22174a8ed
root <root@esilva.com.br>
2024-04-08 23:39:16 -0300

    Avanços 080424 2339

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,9 +2,10 @@ let storeScroll = window.scrollY;
 let htmlTag = document.getElementsByTagName('html');
 document.addEventListener('scroll', () => {
   storeScroll = window.scrollY;
-  if(storeScroll > 20) {
+  if(storeScroll > 1) {
     htmlTag[0].setAttribute('data-scroll', 1);
   } else {
     htmlTag[0].setAttribute('data-scroll', 0);
   }
 });
+
